da2a798a44d16e7a37ab0c2470cad34be66052e9
abliwhite <ajava14@freeuni.edu.ge>
2017-06-10 20:27:00 +0400

    Day 3/Part b
    Register.jsp

--- src/Common/AppCode/ViewTextContainer.java
+++ src/Common/AppCode/ViewTextContainer.java
@@ -4,5 +4,18 @@ public class ViewTextContainer {
 	
 	public static final String LOGIN_VALIDATION_ERROR = "Name of password incorrect. Try again!";
 	public static final String LOGIN_HEAD_VALIDATION_ERROR = "Try again.";
-
+	
+	public static final String REGISTER_PAGE_TITLE = "Create Account";
+	public static final String REGISTER_PAGE_WELCOME_WARNING_MESSAGE = "Create New Account";
+	public static final String REGISTER_PAGE_ERROR_COMMENT_MESSAGE = "Please enter another name and password";
+	
+	private String registerPageWarningError;
+	
+	public ViewTextContainer(String registerPageWarningError){
+		this.registerPageWarningError = registerPageWarningError;
+	}
+	
+	public String getRegisterPageWarningError(){
+		return registerPageWarningError;
+	}	
 }
