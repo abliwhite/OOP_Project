47268c34964c19a54fcf791aaf1f4ecc022ecf55
jonahgeladze <igela14@freeuni.edu.ge>
2017-06-10 16:49:28 +0400

    10June/Part2
    View Text Container added

---
+++ src/Common/AppCode/ViewTextContainer.java
@@ -0,0 +1,8 @@
+package Common.AppCode;
+
+public class ViewTextContainer {
+	
+	public static final String LOGIN_VALIDATION_ERROR = "Name of password incorrect. Try again!";
+	public static final String LOGIN_HEAD_VALIDATION_ERROR = "Try again.";
+
+}
