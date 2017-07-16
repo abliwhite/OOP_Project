e963dd862ee7ac12678492e44191cd28a59383cd
Tornike Bubuteishvili <tbubu14@freeuni.edu.ge>
2017-07-16 16:03:09 -0400

    chat
    added groupchat veiwmodel

--- src/Chat/Models/ViewModels/GroupChatViewModel.java
+++ src/Chat/Models/ViewModels/GroupChatViewModel.java
@@ -1,5 +1,34 @@
 package Chat.Models.ViewModels;
 
+import Account.Models.User;
+import Chat.Models.DbModels.GroupChat;
+
 public class GroupChatViewModel {
 
+	private GroupChat groupChat;
+	private User user;
+	
+	public GroupChatViewModel(GroupChat groupChat, User user){
+		this.setGroupChat(groupChat);
+		this.setUser(user);
+	}
+
+	public GroupChat getGroupChat() {
+		return groupChat;
+	}
+
+	public void setGroupChat(GroupChat groupChat) {
+		this.groupChat = groupChat;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
 }
